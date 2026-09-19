93a584aa36b8505fea291ddfdb303f4e92d01764
Akash Kumar <akashkumar888333@gmail.com>
2026-09-19 21:28:08 +0530

    0000042_function_print_hello_world.cpp

---
+++ 0000042_function_print_hello_world.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+   int printHello()
+{   
+   cout<<"hello"<<endl;
+   return 3;
+}
+   int main()
+{
+   int val=printHello();
+   cout<<"val : "<<val<<endl;
+   return 0;
+}

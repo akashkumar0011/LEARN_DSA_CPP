0a451504211a26944e193a71cbfe46ddfa20bc2f
Akash Kumar <akashkumar888333@gmail.com>
2026-09-19 20:04:06 +0530

    0000027_prime_number.cpp

---
+++ 0000027_prime_number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+   int main()
+{
+   int n=5
+   ;
+   bool isprime=true;
+   for(int i=2; i<=n-1; i++)
+{
+   if(n%i==0)
+{
+   isprime=false;
+   break;
+}
+}
+   if(isprime==true)
+{
+   cout<<"prime number"<<endl;
+}
+   else
+{
+   cout<<"non prime number"<<endl;
+}
+   return 0;
+}
